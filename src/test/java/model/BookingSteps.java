4de1f49ffcf3c5facd1311130b1a79fad76745fd
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-07-15 12:00:52 +0200

    Creating new class BookingSteps for ccmbr feature definition

---
+++ src/test/java/model/BookingSteps.java
@@ -0,0 +1,12 @@
+package model;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+
+public class BookingSteps {
+    @Given("^I want two tickets to New York with AirFrance$")
+    public void iWantTwoTicketsToNewYorkWithAirFrance() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
